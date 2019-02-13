3e2bf5fb5e4185927894579d130afc82667ee486
Nicolai Agersbæk <na@zitcom.dk>
2019-02-13 13:21:35 +0100

    Testing scripts.

--- scripts/pipeline/test.sh
+++ scripts/pipeline/test.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+workingDirName=${PWD##*/}
+projectName=${workingDirName}
+
+#if [[ $1 != "" ]]; then
+#    projectName=$1
+#fi
+
+echo "running tests for project: $projectName"
+
+echo "mkdir -p /go/src/gitlab.zitcom.dk/smartweb/proj"
+echo "mkdir -p /go/src/_/builds/smartweb/proj/$projectName"
+echo "cp -r ${CI_PROJECT_DIR} /go/src/gitlab.zitcom.dk/smartweb/proj/$projectName"
+echo "ln -s /go/src/gitlab.zitcom.dk/smartweb/proj/$projectName /go/src/_/builds/smartweb/proj/$projectName"
+echo "cd /go/src/gitlab.zitcom.dk/smartweb/proj/$projectName"
+
 mkdir -p /go/src/gitlab.zitcom.dk/smartweb/proj
 mkdir -p /go/src/_/builds/smartweb/proj/php-dependency-checker
 cp -r ${CI_PROJECT_DIR} /go/src/gitlab.zitcom.dk/smartweb/proj/php-dependency-checker
